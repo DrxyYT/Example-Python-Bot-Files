c420431acd382ec3f9d93918622cbfcd17cffce6
CFWツDrxy <128531622+DrxyYT@users.noreply.github.com>
2024-02-22 09:48:49 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# You can rename the py file to your choosing. This is a example name. Now, heres the bot code.
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.all()
+intents.message_content = True # Make sure to enable this setting for your bot in the Dev Portal. It needs this to send the messages after the command is sent.
+
+client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix='', intents=intents)
+# ^ change the prefix to your liking :)
+@bot.event
+async def on_ready():
+    print(f'(bot name) is online and ready to go!')
+    await bot.change_presence(activity=discord.Game(name="Testing..."), status=discord.Status.online)
+#Feel free to change the Game status to whatever you like. :)
+
+try:
+    bot.run('YOUR_BOT_TOKEN_HERE')
+except KeyboardInterrupt:
+    print('Nyxx is shutting down...')
+    bot.logout()
+    bot.close()
